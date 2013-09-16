3d5ceb71b1ebe7a54247f091d6c9b23b335252c1
smyx <smyx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-16 03:44:13 +0000

    V0.1
    git-svn-id: https://plugins.svn.wordpress.org/wechat/trunk@773050 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ Readme.txt
@@ -0,0 +1,87 @@
+=== Plugin Name ===
+Contributors: smyx
+Donate link: http://www.smyx.net/wechat.html
+Tags: post,wechat,weixin,widget,微信
+Requires at least: 2.8
+Tested up to: 3.6.1
+Stable tag: 0.1
+
+使用微信搜索Wordpress文章，关键字自定义回复，消息记录和数据分析，微信服务号支持自定义菜单等。
+
+== Description ==
+
+This plugin can achieve search Wordpress post, custom keywords, message records and data analyze, WeChat custom menu. 
+
+使用微信搜索Wordpress文章，关键字自定义回复，消息记录和数据分析，微信服务号支持自定义菜单等。
+
+= 插件介绍 =
+
+演示地址(demo)： http://www.smyx.net/demo/wechat.html
+
+**先看看目前插件的功能：**
+
+1、使用微信搜索Wordpress文章，
+2、后台关键字自定义回复（包括文本、图文、自定义图文、音乐等）
+3、消息记录 （用户与公众平台的互动记录）
+4、数据分析 （包括消息记录统计、热门关键字、活跃用户等）
+5、微信自定义菜单（微信服务号可申请）
+6、用微信发布微博（需要安装[WordPress连接微博](http://www.smyx.net/wp-connect.html) ）
+7、支持wp后台自动更新插件。
+
+**Current Features:**
+
+1. Use WeChat to search Wordoress Posts
+2. custom keywords to reply (include text, image + text, custom image + text, music and etc...)
+3. message records (transcripts betweens clients and public account)
+4. Data Analizy ( included message record，keywords，user activities)
+5. WeChat custom menu ( WeChat Service account should apply)
+6. User could use WeChat to publish Weibo ( need [WP Connect](http://www.smyx.net/wp-connect.html) plugin )
+7. support automaticly update from Wordpress backend.
+
+**仅售：￥99 RMB 或者 $18.5 USD 购买地址：http://www.smyx.net/wechat.html **
+**only：￥99 RMB or $18.5 USD Buy Now: http://www.smyx.net/wechat.html **
+
+[WordPress连接微博专业版](http://www.smyx.net/wp-connect.html) 用户购买本插件享受4.5折优惠，输入上次购买的域名后即可看到优惠。
+[WP Connect Pro](http://www.smyx.net/wp-connect.html) Version User could gain 4.5 Discount，enter your registered domain to get discount details.
+
+= Installation =
+
+= “微信公众平台”开通指南 =
+
+1、打开[微信公众平台](http://mp.weixin.qq.com/) ，点击右上角的“注册”。
+2、登录QQ后，填写信息，其中第一项“帐号名称”，尽量取得好听写，支持中文噢。（注册后不可修改）
+3、选择“普通公众帐号类型”
+4、注册完成后，您可以在“设置”里面，看到您的微信号和二维码，建议把二维码下载下来并上传到您的网站，放在醒要位置。
+5、点击菜单栏的“高级功能”，先进入"编辑模式"，点击“关闭”。之后进入“开发模式“，点击“开启”，再点击下面的“成为开发者”，在“接口配置信息”里面填写上面提示的URL和Token。 
+
+= "WeChat Public Account" Account Opening Instruction =
+1、Open [WeChat Public Account](http://mp.weixin.qq.com/), click on "Register".
+2、Login to your QQ account, provide all your details, please give a nice name for "Account Name" (Notes: This account name cannot be changed after this setting). 
+3、Select"Regular WeChat Public Account"
+4、After registration, you could view your account number and QR code under "Settings". It is a good way to download this QR image and put on your website.
+5、Click "Advance Function", Disable "Editor Model" and go to "Developer Model", enable it, then click on "Become Developer", please fill correct URL and Token. 
+
+= 注意事项 =
+
+本插件需要安装zend组件，目前仅支持安装了PHP5.2.x和PHP5.3.x系列的服务器环境，购买前，请下载测试文件 http://pan.baidu.com/share/link?uk=503347288&shareid=458918 下载后上传到网站根目录，通过 http://您的网站网址/test.php 打开，检查您的服务器是否支持！
+
+== Installation ==
+
+1. Upload the `wechat` folder to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Configure the settings using "wechat" under Settings panel.
+
+下载 连接微信 插件，上传wechat目录及其文件到 "/wp-content/plugins/" 插件目录，在后台管理中激活插件，到设置页面开启功能并设置，以及账号绑定等.
+
+== Screenshots ==
+
+1. 插件后台 - 基本设置
+2. 插件后台 - General Settings
+3. 插件后台 - 自定义回复1
+4. 插件后台 - 自定义回复2
+5. 插件后台 - 消息记录
+6. 插件后台 - 数据分析
+7. 插件后台 - 自定义菜单
+8. 插件后台 - 微信演示
+
+== Changelog ==
---
+++ wechat.php
@@ -0,0 +1,82 @@
+<?php
+/*
+Plugin Name: WeChat (连接微信)
+Author: smyx
+Author URI: http://www.smyx.net/
+Plugin URI: http://www.smyx.net/wechat.html
+Description: This plugin can achieve search Wordpress post, custom keywords, message records and data analyze. (使用微信搜索Wordpress文章，关键字自定义回复，消息记录和数据分析，服务号自定义菜单等。)
+Version: 0.1
+*/
+
+if (!function_exists('installed_zend')) {
+	function installed_zend() {
+		if (version_compare(PHP_VERSION, '5.4', '>=')) {
+			$zend_install_tips = __('Sorry, you cannot use this paid plugin, ZEND is not support for php 5.4.x or above.', 'wechat');
+		} else {
+			$zend_loader_enabled = function_exists('zend_loader_enabled');
+			if ($zend_loader_enabled) {
+				$zend_loader_version = function_exists('zend_loader_version') ? zend_loader_version() : '';
+				if (version_compare($zend_loader_version, '3.3', '>=')) {
+					return (version_compare(PHP_VERSION, '5.3', '>=')) ? '/zend/' : '/';
+				} else {
+					$zend_install_tips = __('Sorry, you cannot use this paid plugin, ZEND version is not up to date, please update to at least 3.3.0<a href="http://www.zend.com/en/products/guard/downloads" target="_blank">View</a>', 'wechat');
+				}
+			} else {
+				if (version_compare(PHP_VERSION, '5.3', '>=')) {
+					$zend_install_tips = __('Sorry, you cannot use this paid plugin, ZEND is not support for php 5.3.x, please contact your server company <a href="http://www.zend.com/en/products/guard/downloads" target="_blank">Zend Guard Loader</a>.', 'wechat');
+				} else {
+					$zend_install_tips = __('Sorry, you cannot use this paid plugin, please contact your server company <a href="http://www.zend.com/en/products/guard/downloads" target="_blank">Zend Optimizer</a>.', 'wechat');
+				} 
+			}
+		}
+		return array('error' => $zend_install_tips);
+	} 
+}
+
+function wp_wechat_init() {
+    load_plugin_textdomain( 'wechat', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+}
+add_action( 'init', 'wp_wechat_init' );
+
+add_action('admin_menu', 'wp_wechat_add_page');
+function wp_wechat_add_page() {
+	$weixin_url = plugins_url('wechat');
+	if (function_exists('add_menu_page')) {
+		add_menu_page(__('WeChat', 'wechat'), __('WeChat', 'wechat'), 'manage_options', 'wechat', 'wp_wechat_do_page', $weixin_url .'/images/small-weixin.gif');
+	} 
+} 
+add_action('plugin_action_links_' . plugin_basename(__FILE__), 'wp_wechat_plugin_actions');
+function wp_wechat_plugin_actions ($links) {
+    $new_links = array();
+    $new_links[] = '<a href="admin.php?page=wechat">' . __('Settings') . '</a>';
+    return array_merge($new_links, $links);
+}
+// 设置 Setting
+function wp_wechat_do_page() {
+	$weixin_url = plugins_url('wechat');
+	$installed_zend = installed_zend();
+	if (is_array($installed_zend)) {
+		echo '<div class="updated">';
+		echo '<p><strong>' . $installed_zend . '</strong></p>';
+		echo '</div>';
+	}
+?>
+<div class="wrap">
+  <h2><img src="<?php echo $weixin_url .'/images/icon_weixin.png';?>" /><?php _e('WeChat', 'wechat');?></h2>
+  <h3><?php _e('Plugin Summary', 'wechat');?></h3>
+  <p><?php _e('Current Features:', 'wechat');?></p>
+  <p><?php _e('1. Use WeChat to search Wordoress Posts', 'wechat');?></p>
+  <p><?php _e('2. custom keywords to reply (include text, image + text, custom image + text, music and etc...)', 'wechat');?></p>
+  <p><?php _e('3. message records (transcripts betweens clients and public account)', 'wechat');?></p>
+  <p><?php _e('4. Data Analizy ( included message record，keywords，user activities)', 'wechat');?></p>
+  <p><?php _e('5. WeChat custom menu ( WeChat Service account should apply)', 'wechat');?></p>
+  <p><?php _e('6. User could use WeChat to publish Weibo ( need <a href="http://www.smyx.net/wp-connect.html" target="_blank">WP Connect</a> plugin )', 'wechat');?></p>
+  <p><?php _e('7. support automaticly update from Wordpress backend.', 'wechat');?></p>
+  <h3><?php _e('only：￥99 RMB or $18.5 USD Buy Now: <a href="http://www.smyx.net/wechat.html" target="_blank">http://www.smyx.net/wechat.html</a>', 'wechat');?></h3>
+  <p><?php _e('<a href="http://www.smyx.net/wp-connect.html" target="_blank">WP Connect Pro</a> Version User could gain 4.5 Discount，enter your registered domain to get discount details.', 'wechat');?></p>
+  <h3><?php _e('"WeChat Public Account" Account Opening Instruction', 'wechat');?></h3>
+  <p><?php _e('1、Open <a href="http://mp.weixin.qq.com/" target="_blank">WeChat Public Account</a>, click on "Register".<br />2、Login to your QQ account, provide all your details, please give a nice name for "Account Name" (Notes: This account name cannot be changed after this setting). <br />3、Select"Regular WeChat Public Account"<br />4、After registration, you could view your account number and QR code under "Settings". It is a good way to download this QR image and put on your website.<br />5、Click "Advance Function", Disable "Editor Model" and go to "Developer Model", enable it, then click on "Become Developer", please fill correct URL and Token.', 'wechat');?>
+  </p>
+</div>
+<?php
+} 
